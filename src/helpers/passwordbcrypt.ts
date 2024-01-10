bf9d1d6f1e71b7b56bbf1c619d4dfe4f19606c9e
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-01-10 14:38:59 +0600

    [DONE] jwt and password bcyript done.

---
+++ src/helpers/passwordbcrypt.ts
@@ -0,0 +1,23 @@
+import bcrypt from "bcrypt"
+
+const saltRounds = 10
+
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  return hashedPassword
+}
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  return await bcrypt.compare(password, hashedPassword)
+}
+
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  return await bcrypt.compare(password, hashedPassword)
+}
